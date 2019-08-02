f7c17bfa8e2e631934b9c244ab43690ec4715d3a
David Palencia <brageanth@gmail.com>
2019-08-01 19:21:02 -0500

    commit

---
+++ precache-manifest.0de63588f0d4bba785338bae92b6d790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1eea3cadba2b9ec45c",
+    "url": "/0.css"
+  },
+  {
+    "revision": "78e06f8049650beb0e2864e40fd4b756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61df557077156dfc74bd",
+    "url": "/static/js/2.29202066.chunk.js"
+  },
+  {
+    "revision": "0c1eea3cadba2b9ec45c",
+    "url": "/static/js/main.85173e0f.chunk.js"
+  },
+  {
+    "revision": "73dac8e2e8de609c4bc6",
+    "url": "/static/js/runtime~main.3e812b80.js"
+  }
+]);
