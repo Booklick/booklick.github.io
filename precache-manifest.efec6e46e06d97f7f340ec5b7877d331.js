a04732f397d459710eae9ffc39b664901cfc67d9
David Mesa <david@booklick.co>
2020-04-07 11:22:28 -0500

    URLs

---
+++ precache-manifest.efec6e46e06d97f7f340ec5b7877d331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a97ec8dc8b615e5a8c1",
+    "url": "/0.css"
+  },
+  {
+    "revision": "39680151dc5e6ac19d807f4fac4f8a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853a71ba935fdde89b97",
+    "url": "/static/js/2.4ddece14.chunk.js"
+  },
+  {
+    "revision": "5a97ec8dc8b615e5a8c1",
+    "url": "/static/js/main.529ece8e.chunk.js"
+  },
+  {
+    "revision": "73dac8e2e8de609c4bc6",
+    "url": "/static/js/runtime~main.3e812b80.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.856956c7b34b5a1ed7a6860ce967c999.js"
+  "/precache-manifest.efec6e46e06d97f7f340ec5b7877d331.js"
 );
 
 self.addEventListener('message', (event) => {
